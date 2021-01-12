ae787a653e393371d30989268bf3a2f9219e3890
Antonio Cituk <tonycituk@gmail.com>
2021-01-12 13:55:31 -0500

    new features needed

---
+++ web/services/anotaciones.service.js
@@ -0,0 +1,22 @@
+'use-strict';
+var anotaciones;
+
+//Obtener todas las anotaciones
+function getAllAnotaciones() {
+
+}
+
+//Agregar una anotacion
+function addAnotacion() {
+
+}
+
+//Actualizar una anotacion
+function updateAnotacion() {
+
+}
+
+//Eliminar una anotacion
+function deleteAnotacion() {
+
+}
