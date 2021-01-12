51b6448f2bd2cd57d9a8b6c3ae93548d07cd13e3
Antonio Cituk <tonycituk@gmail.com>
2021-01-12 16:37:06 -0500

    getAnotaciones + deleteAnotacion

--- web/services/anotaciones.service.js
+++ web/services/anotaciones.service.js
@@ -5,7 +5,9 @@ let URL = "https://hunnigan-dev.herokuapp.com/";
 
 //Obtener todas las anotaciones
 function getAllAnotaciones() {
-
+    fetch(URL+'anotaciones')
+    .then(response  => response.json())
+    .then(data => anotaciones = data);
 }
 
 //Agregar una anotacion
@@ -19,6 +21,19 @@ function updateAnotacion() {
 }
 
 //Eliminar una anotacion
-function deleteAnotacion() {
-
+function deleteAnotacion(id) {
+    let req = new XMLHttpRequest();
+    req.open('DELETE', URL + 'anotacion/' + id, true);
+    req.onreadystatechange = function () {
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                console.info(req.responseText);
+                getAllAnotaciones();
+            }
+            else {
+                console.error("Error loading page\n");
+            }
+        }
+    };
+    req.send(null);
 }
